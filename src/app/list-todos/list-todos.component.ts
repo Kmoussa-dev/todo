dd1995aceb1a8c1ee0c7f9a0863e6501bd0a3358
kmoussa-dev <konemoussamohamed@gmail.com>
2021-09-29 10:10:18 +0200

    list-todos

--- src/app/list-todos/list-todos.component.ts
+++ src/app/list-todos/list-todos.component.ts
@@ -1,5 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+export class Todo {
+  constructor(
+    public id: number,
+    public description: string,
+    public done: boolean,
+    public targetDate: Date
+  ) {
+
+  }
+}
 @Component({
   selector: 'app-list-todos',
   templateUrl: './list-todos.component.html',
@@ -8,10 +18,17 @@ import { Component, OnInit } from '@angular/core';
 export class ListTodosComponent implements OnInit {
 
   todos = [
-    {id : 1, description: 'Learn the QURAN' },
-    {id : 2, description: 'Learn  an expert at angular'},
-    {id : 3, description: 'Visit France'}
+    new Todo(1, 'Learn to Dance', false, new Date()),
+    new Todo(2, 'Become an expert at angular', false, new Date()),
+    new Todo(3, 'Visit France', false, new Date())
   ];
+
+
+  // todos = [
+  //   {id : 1, description: 'Learn the QURAN' },
+  //   {id : 2, description: 'Learn  an expert at angular'},
+  //   {id : 3, description: 'Visit France'}
+  // ];
   // todo = {
   //   id : 1,
   //   description : 'Learn the QURAN'
